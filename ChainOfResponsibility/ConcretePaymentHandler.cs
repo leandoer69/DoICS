59d685dbb75aa13a6b0c9f983ec4498214146a0c
leandoer69 <glebuxa2004@gmail.com>
2025-01-08 22:47:38 +0300

    Added Chain of responsibility pattern

---
+++ ChainOfResponsibility/ConcretePaymentHandler.cs
@@ -0,0 +1,33 @@
+﻿class BankPaymentHandler : PaymentHandler
+{
+    public override void Handle(Receiver receiver)
+    {
+        if (receiver.BankTransfer == true)
+            Console.WriteLine("Выполняем банковский перевод");
+        else if (Successor != null)
+            Successor.Handle(receiver);
+    }
+}
+
+class MoneyPaymentHandler : PaymentHandler
+{
+    public override void Handle(Receiver receiver)
+    {
+        if (receiver.MoneyTransfer == true)
+            Console.WriteLine("Выполняем перевод через системы денежных переводов");
+        else if (Successor != null)
+
+            Successor.Handle(receiver);
+    }
+}
+
+class PayPalPaymentHandler : PaymentHandler
+{
+    public override void Handle(Receiver receiver)
+    {
+        if (receiver.PayPalTransfer)
+            Console.WriteLine("Выполняем перевод через PayPal");
+        else if (Successor != null)
+            Successor.Handle(receiver);
+    }
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,50 @@
+﻿
+class Receiver
+{
+    // банковские переводы
+    public bool BankTransfer { get; set; }
+    // денежные переводы - WesternUnion, Unistream
+    public bool MoneyTransfer { get; set; }
+    // перевод через PayPal
+    public bool PayPalTransfer { get; set; }
+    public Receiver(bool bt, bool mt, bool ppt)
+    {
+        BankTransfer = bt;
+        MoneyTransfer = mt;
+        PayPalTransfer = ppt;
+    }
+}
+
+abstract class PaymentHandler
+{
+    public PaymentHandler Successor { get; set; }
+    public abstract void Handle(Receiver receiver);
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Receiver receiver = new Receiver(false, true, true);
+        PaymentHandler bankPaymentHandler = new BankPaymentHandler();
+        PaymentHandler moneyPaymentHandler = new MoneyPaymentHandler();
+        PaymentHandler paypalPaymentHandler = new PayPalPaymentHandler();
+
+        // старая цепочка переводов
+        bankPaymentHandler.Successor = paypalPaymentHandler;
+        paypalPaymentHandler.Successor = moneyPaymentHandler;
+        bankPaymentHandler.Handle(receiver);
+
+
+        Receiver receiver1 = new Receiver(true, true, true);
+        PaymentHandler bankPaymentHandler1 = new BankPaymentHandler();
+        PaymentHandler moneyPaymentHandler1 = new MoneyPaymentHandler();
+        PaymentHandler paypalPaymentHandler1 = new PayPalPaymentHandler();
+
+        // новая цепочка переводов
+        moneyPaymentHandler1.Successor = bankPaymentHandler1;
+        bankPaymentHandler1.Successor = paypalPaymentHandler1;
+
+        bankPaymentHandler1.Handle(receiver1);
+    }
+}
